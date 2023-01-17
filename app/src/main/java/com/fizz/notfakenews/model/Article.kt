d632bdaefb90e36670da6be8507ed5689446fa31
Sheikh Mustafizur Rahman <sheikh.mustafizur.rahman@bjitacademy.com>
2023-01-17 16:32:45 +0600

    Used fragment and showing fragment view. Specific website is visible now

---
+++ app/src/main/java/com/fizz/notfakenews/model/Article.kt
@@ -0,0 +1,12 @@
+package com.fizz.notfakenews.model
+
+data class Article(
+    val title: String?,
+    val author: String?,
+    val content: String?,
+    val description: String?,
+    val publishedAt: String?,
+    val source: Source,
+    val url: String?,
+    val urlToImage: String?
+)
---
+++ app/src/main/java/com/fizz/notfakenews/model/Source.kt
@@ -0,0 +1,6 @@
+package com.fizz.notfakenews.model
+
+data class Source(
+    val id: String?,
+    val name: String?
+)
