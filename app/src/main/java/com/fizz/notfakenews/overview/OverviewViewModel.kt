20dc31b3cf941f9797b9369702dc1b92710963eb
MustafizurRman <sheikh.mustafizur.rahman@g.bracu.ac.bd>
2023-01-19 01:30:34 +0600

    Just Recycler view and Tab view Added

--- app/src/main/java/com/fizz/notfakenews/overview/OverviewViewModel.kt
+++ app/src/main/java/com/fizz/notfakenews/overview/OverviewViewModel.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.fizz.notfakenews.model.Article
 import com.fizz.notfakenews.network.NewsApi
@@ -13,10 +12,10 @@ import kotlinx.coroutines.launch
 
 private const val TAG= "OverViewModel"
 
-enum class NewsApiStatus { LOADING, ERROR, DONE }
+//enum class NewsApiStatus { LOADING, ERROR, DONE }
 class OverviewViewModel(application: Application):AndroidViewModel(application) {
-    private val _status = MutableLiveData<NewsApiStatus>()
-    val status: LiveData<NewsApiStatus> = _status
+    //private val _status = MutableLiveData<NewsApiStatus>()
+    //val status: LiveData<NewsApiStatus> = _status
     private val _NewsData=MutableLiveData<List<Article>>()
     val newsData: LiveData<List<Article> > = _NewsData
 
